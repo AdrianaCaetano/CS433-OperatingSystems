8dbf2c39438ded42b10376cef1ae449946876211
Adriana Caetano <52258921+AdrianaCaetano@users.noreply.github.com>
2020-10-07 09:14:57 -0700

    Assign1 main

--- Prog1/main.cpp
+++ Prog1/main.cpp
@@ -1,54 +1,61 @@
 /*
- * Programming Assignment 2 - CS 433
- * Description: Unix Shell and History Feature
- * Author: Adriana Caetano and Ryan Pisuena
- * Date: 10/14/2020
+ * Programming Assignment 1 - CS 433
+ * Description: ReadyQueue implementation to control processes and test performance
+ * Author: Adriana Caetano
+ * Date: 09/23/2020
  * File: main.cpp
  */
 
+
+#include <cstdlib>
 #include <iostream>
-#include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
-#include "Support.h"
+#include "PCB.h"
+#include "ReadyQueue.h"
 
-#define MAX_LINE 80 /* The max length command */
+int main(int argc, char* argv[]) {
+        //Print basic information about the program
+        std::cout << std::endl;
+        std::cout << "======================================================================" << std::endl << std::endl;
+        std::cout << "CS 433 Programming assignment 1" << std::endl;
+        std::cout << "Author: Adriana Caetano" << std::endl;
+        std::cout << "Date: 09/23/2020" << std::endl;
+        std::cout << "Course: CS433 (Operating Systems)" << std::endl;
+        std::cout << "Description : Program to implement a priority ready queue of processes" << std::endl << std::endl;
+        std::cout << "======================================================================" << std::endl << std::endl;
 
-int main(void) {
-        Support s;
-        s.show_header();
 
-        char* input;
-        char** command[MAX_LINE / 2 + 1]; /* command line arguments */
-        pid_t child_pid;
-        int should_run = 1; /* flag to determine when to exit the program */
+        // Code for Test 1
+        std::cout << "---------- Performing Test 1 ----------" << std::endl << std::endl;
 
-        while (should_run) {
-                printf("osh> ");
-                fflush(stdout);
+        std::cout << std::endl;
 
-                // parse command line. Get command, args, redirect
+        // seed time for rand()
+        srand(time(NULL));
 
-                // if (cmd == exit) exit();
+        //Create a ReadyQueue
+        ReadyQueue q1;
 
-                /*
-                 * After reading user input, the steps are:
-                 * (1) fork a child process using fork()
-                 * (2) the child process will invoke execvp()
-                 * (3) parent will invoke wait() unless command included &
-                 */
-         /*
-                p = fork();
-                if (p == 0) {
-                        execvp (cmd, args);
-                } else {
-                        if (cmd does not end with &) {
-                                wait();
-                }
-        */
 
-                should_run = 0;
-        }
+        /* Perform test 1
+         * Create a ReadyQueue. Add specific processes, remove the highest priority process,
+         * and display queue elements after each removal.
+         */
+        q1.performTest1();
+        std::cout << std::endl;
+
+        // Code for Test 2
+        std::cout << "---------- Performing Test 2 ----------" << std::endl << std::endl;
+        std::cout << std::endl;
+
+        /* Perform test 2
+         * Evaluate performance of the ReadyQueue implementation inserting and removing processes.
+         * Measure the time to do 1,000,000 iterations and present results.
+         */
+        q1.performTest2();
+        std::cout << std::endl;
 
         std::cout << "---------- End of Program ----------" <<std::endl;
 
