a30c52324fa95a8b4f5940ae663f37224e220c7c
Adriana Caetano <52258921+AdrianaCaetano@users.noreply.github.com>
2020-10-07 08:07:44 -0700

    Create Support.h

---
+++ Prog2/Support.h
@@ -0,0 +1,24 @@
+/*
+ * Programming Assignment 2 - CS 433
+ * Description: Unix Shell and History Feature
+ * Author: Adriana Caetano and Ryan Pisuena
+ * Date: 10/14/2020
+ * File: Support.h
+ */
+
+#ifndef SUPPORT_H
+#define SUPPORT_H
+
+class Support {
+
+        public:
+
+        // Show header with basic information about this program
+        void show_header();
+
+        // Take a string as input and parse it
+        // @return array of commands
+        char **get_input(char *input);
+};
+
+#endif
