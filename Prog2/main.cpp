4d42d4452938e6cd65bc61b2a49b603738342f71
Ryan Pisuena <Pisuenaryan@gmail.com>
2020-10-10 14:25:46 -0700

    spaces

--- Prog2/main.cpp
+++ Prog2/main.cpp
@@ -22,38 +22,52 @@ using namespace support;
 
 int main(void) {
 
-        char input[MAX_LINE];
         char* commands[MAX_LINE/2 + 1]; /* command line arguments */
+  char input[MAX_LINE];
+  char* commands[MAX_LINE/2 + 1]; /* command line arguments */
 	char* last_cmd[MAX_LINE/2 + 1]; /* hold history command */
-	int should_run = 1; /* flag to determine when to exit the program */
+	int num_args = 0; 		/* hold number of command arguments */
+	int should_run = 1; 		/* flag to determine when to exit the program */
 
-        show_header();
+  show_header();
 	cout << "This program simulates a shell.\n" ;
 	cout << "Please input your command.\n\n";
 
 	while(should_run) {
 		printf("osh->");
 		fflush(stdout);
+<<<<<<< HEAD
 
                 // get command
 		fgets(input, MAX_LINE, stdin);
 	//	printf("\n");
-		
+
 		// parse command
 		split_command(input, commands);
 
-		if (strcmp(commands[0], "exit") == 0 ) { 
-			exit(0); 
+		if (strcmp(commands[0], "exit") == 0 ) {
+			exit(0);
 		}
-		
+
 		// DELETE
-		for (auto i = 0; i < sizeof(commands) ; i++ ) { 
+		for (auto i = 0; i < sizeof(commands) ; i++ ) {
 			printf("%s", commands[i]);
 		}
-	
+
 	//	should_run = execute_command(commands);
 
 
+=======
+		
+		// get input from the user
+		fgets(input, MAX_LINE, stdin);
+
+		// parse command
+		num_args = split_command(input, commands);
+		
+		//execute command
+		should_run = execute_command(commands, num_args);
+>>>>>>> c1d0daa7aa85599bd6b984b3b2c6b088b1ce411b
 	}
 
         cout << "---------- End of Program ----------\n";
