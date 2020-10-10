4e5cefb19b57490bd632d17c51ddd9b3f1e2bab8
Ryan Pisuena <Pisuenaryan@gmail.com>
2020-10-10 14:31:58 -0700

    comm

--- Prog2/main.cpp
+++ Prog2/main.cpp
@@ -26,8 +26,7 @@ int main(void) {
   char input[MAX_LINE];
   char* commands[MAX_LINE/2 + 1]; /* command line arguments */
 	char* last_cmd[MAX_LINE/2 + 1]; /* hold history command */
-	int num_args = 0; 		/* hold number of command arguments */
-	int should_run = 1; 		/* flag to determine when to exit the program */
+	int should_run = 1; /* flag to determine when to exit the program */
 
   show_header();
 	cout << "This program simulates a shell.\n" ;
@@ -36,7 +35,6 @@ int main(void) {
 	while(should_run) {
 		printf("osh->");
 		fflush(stdout);
-<<<<<<< HEAD
 
                 // get command
 		fgets(input, MAX_LINE, stdin);
@@ -57,17 +55,6 @@ int main(void) {
 	//	should_run = execute_command(commands);
 
 
-=======
-		
-		// get input from the user
-		fgets(input, MAX_LINE, stdin);
-
-		// parse command
-		num_args = split_command(input, commands);
-		
-		//execute command
-		should_run = execute_command(commands, num_args);
->>>>>>> c1d0daa7aa85599bd6b984b3b2c6b088b1ce411b
 	}
 
         cout << "---------- End of Program ----------\n";
