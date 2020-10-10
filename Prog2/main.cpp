118dc9e883e54788099d26bec9126304862ed48c
Ryan Pisuena <Pisuenaryan@gmail.com>
2020-10-10 14:32:04 -0700

    Revert "comm"
    This reverts commit 4e5cefb19b57490bd632d17c51ddd9b3f1e2bab8.

--- Prog2/main.cpp
+++ Prog2/main.cpp
@@ -26,7 +26,8 @@ int main(void) {
   char input[MAX_LINE];
   char* commands[MAX_LINE/2 + 1]; /* command line arguments */
 	char* last_cmd[MAX_LINE/2 + 1]; /* hold history command */
-	int should_run = 1; /* flag to determine when to exit the program */
+	int num_args = 0; 		/* hold number of command arguments */
+	int should_run = 1; 		/* flag to determine when to exit the program */
 
   show_header();
 	cout << "This program simulates a shell.\n" ;
@@ -35,6 +36,7 @@ int main(void) {
 	while(should_run) {
 		printf("osh->");
 		fflush(stdout);
+<<<<<<< HEAD
 
                 // get command
 		fgets(input, MAX_LINE, stdin);
@@ -55,6 +57,17 @@ int main(void) {
 	//	should_run = execute_command(commands);
 
 
+=======
+		
+		// get input from the user
+		fgets(input, MAX_LINE, stdin);
+
+		// parse command
+		num_args = split_command(input, commands);
+		
+		//execute command
+		should_run = execute_command(commands, num_args);
+>>>>>>> c1d0daa7aa85599bd6b984b3b2c6b088b1ce411b
 	}
 
         cout << "---------- End of Program ----------\n";
