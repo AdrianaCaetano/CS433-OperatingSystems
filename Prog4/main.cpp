35c7d6f23d5036d6d41c0e022e97fc453ea341cf
Adriana Caetano <52258921+AdrianaCaetano@users.noreply.github.com>
2020-11-10 10:14:11 -0800

    Starter code

---
+++ Prog4/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * Programming Assignment 4 - CS 433
+ * Description: Producer-Consumer Problem
+ * Author: Adriana Caetano & Ryan Pisuena
+ * Date: 11/25/2020
+ * File: main.cpp
+ */
+
+#include "buffer.h"
+#include "functions.h"
+
+int main(int argc, char* argv[])
+{
+    Functions::show_header();
+
+    // 1. Get command line arguments argv[1], argv[2], argv[3]
+
+
+    // 2. Initialize buffer
+    Buffer buffer;
+
+    // 3. Create producer thread(s)
+
+
+    // 4. crete consumer thread(s)
+
+
+    // 5. Sleep
+
+
+    // 6. Exit
+
+
+    return 0;
+}
