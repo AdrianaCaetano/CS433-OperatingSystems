85e30f3f99ba763fa029766932bc6424c86a398c
Adriana Caetano <52258921+AdrianaCaetano@users.noreply.github.com>
2020-12-03 11:32:27 -0800

    Update functions to support parsing cmd line

--- Prog5/functions.cpp
+++ Prog5/functions.cpp
@@ -7,42 +7,41 @@
  */
 
 #include <cctype> //isdigit()
+#include <cmath> //log2()
 #include <iostream>
 #include <string>
 
 #include "functions.h"
 
-using namespace std;
-
 // Show header with program basic information
 void Functions::show_header()
 {
-    cout << "\n=================================================================================\n\n";
-    cout << "CS 433 Programming Assignemnt 5\n";
-    cout << "Author: Adriana Caetano and Ryan Pisuena\n";
-    cout << "Date: 12/11/2020\n";
-    cout << "Course: CS433 (Operating Systems)\n";
-    cout << "Description: Analysis of Page Replacement\n";
-    cout << "\n=================================================================================\n\n";
+    std::cout << "\n=================================================================================\n\n";
+    std::cout << "CS 433 Programming Assignemnt 5\n";
+    std::cout << "Author: Adriana Caetano and Ryan Pisuena\n";
+    std::cout << "Date: 12/11/2020\n";
+    std::cout << "Course: CS433 (Operating Systems)\n";
+    std::cout << "Description: Analysis of Page Replacement\n";
+    std::cout << "\n=================================================================================\n\n";
+
 }
 
-
 // Check if input is a number
-bool Functions::check_number(string input)
+bool Functions::check_number(std::string input)
 {
-    for (int i = 0; i < input.length(); i++)
-    { 
+    for (unsigned int i = 0; i < input.length(); i++)
+    {
         if (isdigit(input[i]) == false) { return false; }
     }
     return true;
 }
 
 // Show usage to user
-string Functions::usage (char* name)
+std::string Functions::usage (char* name)
 {
-    string input = name; //convert c-string to string
-    string message = "Usage: " ;
-    string parameters = " [# page size] [# physical memory size]\n\n";
+    std::string input = name; //convert c-string to string
+    std::string message = "Usage: " ;
+    std::string parameters = " [# page size] [# physical memory size]\n\n";
 
     return message + input + parameters;
 }
@@ -51,28 +50,26 @@ string Functions::usage (char* name)
 void Functions::valid_parameters(int argc, char* name)
 {
     // Check if number of parameters is correct
-    if (argc != 3) 
-    { 
-        cerr << "You have entered too few parameters to run the program.\n";
-        cerr << "You must enter two command-line arguments:\n";
-        cerr << " [# page size] = between 256 and 8192, inclusive (in bytes)\n";
-        cerr << " [# physical memory size] = between 4 and 64, inclusive (in megabytes)\n";
-        
-        cerr << Functions::usage(name);
+    if (argc != 3)
+    {
+        std::cerr << "You have entered too few parameters to run the program.\n";
+        std::cerr << "You must enter two command-line arguments:\n";
+        std::cerr << " [# page size] = between 256 and 8192, inclusively (in bytes)\n";
+        std::cerr << " [# physical memory size] = between 4 and 64, inclusively (in megabytes)\n";
 
-        exit(0);
+        std::cerr << Functions::usage(name);
+
+        exit(1);
     }
-    return;    
+    return;
 }
 
-void Functions::show_error(string message, char* name)
+void Functions::show_error(std::string message, char* name)
 {
-    cerr << message << endl;
-    cerr << Functions::usage(name);
-    exit(0);
+    std::cerr << message << std::endl;
+    std::cerr << Functions::usage(name);
+    exit(1);
 }
-
-// TODO FIX FROM HERE FOR PROG 5
 
 // Parse command line and return a struct with integer values for the parameters
 Parameters Functions::get_parameters(int argc, char** argv)
@@ -81,51 +78,93 @@ Parameters Functions::get_parameters(int argc, char** argv)
     Functions::valid_parameters(argc, argv[0]);
 
     Parameters p; // struct to hold parameters' values
-    
-    // argv[1] = how long the main thread sleep before terminating(in seconds)
-    string input = argv[1];
+
+    // argv[1] = the number of the page size(in bytes)
+    std::string input = argv[1];
     if (Functions::check_number(input))
-    { 
-        p.sleep_time = stoi(input);
-        if (p.sleep_time < 1) 
-        { 
-            Functions::show_error("Sleep time must be a positive nonzero integer.", argv[0]);
+    {
+        p.page_size = stoi(input);
+        if (Functions::isPowerOfTwo(p.page_size))
+        {
+            if (p.page_size < 256 || p.page_size > 8192)
+            {
+                Functions::show_error("Page size must be a between 256 (2^8) and 8192 (2^13) bytes, inclusively.", argv[0]);
+            }
+        }
+        else
+        {
+            Functions::show_error("Page size must be a power of 2", argv[0]);
         }
     }
-    else 
-    { 
-        Functions::show_error("Sleep time input is not an integer.", argv[0]);
-    } 
+    else
+    {
+        Functions::show_error("Page size input is not an integer.", argv[0]);
+    }
 
-    // argv[2] = the number of producer threads
+    // argv[2] = the number of the physical memory (in MB)
     input = argv[2];
     if (Functions::check_number(input))
-    { 
-        p.num_producers = stoi(input);
-        if (p.num_producers < 0) 
+    {
+        p.physical_memory_size = stoi(input);
+        if (Functions::isPowerOfTwo(p.physical_memory_size))
         {
-            Functions::show_error("Number of producer threads must be zero or a positive integer.", argv[0]);
+            if (p.physical_memory_size < 4 || p.physical_memory_size > 64)
+            {
+                Functions::show_error("Physical memory size must be between 4MB (2^22) and 64MB (2^26), inclusively.", argv[0]);
+            }
+        }
+        else
+        {
+            Functions::show_error("Physical memory must be a power of 2", argv[0]);
         }
     }
-    else 
-    {    
-        Functions::show_error("Number of producer threads is not an integer.", argv[0]);
+    else
+    {
+        Functions::show_error("Physical memory size input is not an integer.", argv[0]);
     }
-    
-    // argv[3] = the number of consumer threads
-    input = argv[3];
-    if (Functions::check_number(input))
-    { 
-        p.num_consumers = stoi(input);
-        if (p.num_consumers < 0) 
-        {
-            Functions::show_error("Number of consumer threads must be zero or a positive integer.", argv[0]);
-        }
-    }
-    else 
-    {    
-        Functions::show_error("Number of consumer threads is not an integer.", argv[0]);
-    }
+
+    // transform to bytes and calculate remain Parameter p members
+    Functions::calculate_parameters(& p);
+
+    std::cout << "Page size = " << p.page_size << " bytes" << std::endl;
+    std::cout << "Physical Memory size = " << p.physical_memory_size << " bytes" << std::endl;
+    std::cout << "Number of pages = " << p.num_pages << std::endl;
+    std::cout << "Number of physical frames = " << p.num_frames << std::endl;
 
     return p;
 }
+
+// calculate frames in bytes
+void Functions::calculate_parameters(Parameters* p)
+{
+    //convert physical memory size from MB to bytes (= 2^20)
+    p->physical_memory_size = p->physical_memory_size << 20;
+
+    // calculate number of pages and frames
+
+    // 27-bit logical memory (128 MB logical memory assumed by the assignment)
+    int logic_mem_bits = 27;
+
+    // Num of bits for physical memory addresses, calculated from physical memory size
+    // e.g. 24 bits for 16 MB memory
+    int phys_mem_bits = std::log2(p->physical_memory_size);
+
+    // Num of bits for page offset, calculated from page size, e.g. 12 bits for 4096 byte page
+    int page_offset_bits = std::log2(p->page_size);
+
+    // Number of pages in logical memory = 2^(logic_mem_bits - page_bit)
+    p->num_pages = 1 << (logic_mem_bits - page_offset_bits);
+
+    // Number of free frames in physical memory = 2^(phys_mem_bits - page_offset_bits)
+    p->num_frames = 1 << (phys_mem_bits - page_offset_bits);
+
+}
+
+// Check if an integer is power of 2
+bool Functions::isPowerOfTwo(int x)
+{
+        /* First x in the below expression is for the case when x is 0 */
+        return x && (!(x & (x - 1)));
+}
+
+
