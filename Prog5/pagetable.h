2d73434e8f76cf3c21a7cb002d8cf489b740949d
Adriana Caetano <52258921+AdrianaCaetano@users.noreply.github.com>
2020-12-03 11:35:24 -0800

    Include file header

--- Prog5/pagetable.h
+++ Prog5/pagetable.h
@@ -1,4 +1,14 @@
-#pragma once
+/*
+ * Programming Assignment 5 - CS 433
+ * Description: Analysis of Page Replacement
+ * Author: Adriana Caetano & Ryan Pisuena
+ * Date: 12/11/2020
+ * File: pagetable.h
+ */
+
+#ifndef PAGETABLE_H
+#define PAGETABLE_H
+
 
 // Remember to add comments to your code
 
@@ -6,12 +16,12 @@
 class PageEntry
 {
 public:
-	// Physical frame number for a given page
-	int frame_num;
-	// valid bit represents whether a page is in the physical memory
-	bool valid = false;
-	// dirty bit represents whether a page is changed
-	bool dirty = false;
+        // Physical frame number for a given page
+        int frame_num;
+        // valid bit represents whether a page is in the physical memory
+        bool valid = false;
+        // dirty bit represents whether a page is changed
+        bool dirty = false;
 };
 
 
@@ -20,5 +30,8 @@ public:
  */
 class PageTable
 {
-	// TODO: Add your implementation of the page table here
+        // TODO: Add your implementation of the page table here
 };
+
+#endif
+
