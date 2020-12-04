d1026d4c85408877acc1f30be04e8f714947eb80
Adriana Caetano <52258921+AdrianaCaetano@users.noreply.github.com>
2020-12-04 13:21:32 -0800

    Create page table from file

--- Prog5/pagetable.h
+++ Prog5/pagetable.h
@@ -9,19 +9,33 @@
 #ifndef PAGETABLE_H
 #define PAGETABLE_H
 
+#include <stack>
+#include <string>
 
-// Remember to add comments to your code
+#include "functions.h"
 
 // A page table entry
 struct PageEntry
 {
-
+    // Logical address from file
+    int logical_add;
+    // Page Number
+    int page_num;
     // Physical frame number for a given page
     int frame_num;
     // valid bit represents whether a page is in the physical memory
     bool valid = false;
     // dirty bit represents whether a page is changed
     bool dirty = false;
+
+    // Contructor
+    PageEntry() {};
+    PageEntry(int logical, int page)
+    {
+        logical_add = logical;
+        page_num = page;
+        frame_num = 0;
+    }
 };
 
 
@@ -32,18 +46,40 @@ struct PageEntry
 class PageTable
 {
     private:
-        int size;
-        PageEntry* page_table;
+        std::stack <PageEntry*> page_table;
 
     public:
         // Constructors
         PageTable();
-        PageTable(int size);
 
         // Destructor
         ~PageTable();
 
+        // Return the size of the page table
+        int get_size();
+
+        // Open file, read logical address, return a page entry
+        void open_file(std::string file_name, int page_size);
+
+        /* Perform test 1
+         * In the first test, your program should read and run the simulation for a small list 
+         * of logical addresses (“small_refs.txt”). Assuming an initial empty physical memory, 
+         * for each logical address in the list, print out its logical page #, physical frame #, 
+         * and whether it caused a page fault or not.
+         */
+         void test1(Parameters p);
+
+        /* Perform test 2
+         * In the second test, assuming an initial empty physical memory, your program should read 
+         * and run the simulation for a large list of logical addresses (“large_refs.txt”), and 
+         * collect and print the following statistics for different algorithms: 
+         * – The total number of memory references.
+         * – The total number of page faults.
+         * – The total number of page replacements.
+         * – The total time it took the simulator to produce the results.
+         */
+         void test2(Parameters p);
+
 };
 
 #endif
-
